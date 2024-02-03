1ce2b2b029c619d004996b3664dcd6c83b0956c2
AiCorein <melodyecho@glowmem.com>
2024-02-03 16:32:28 +0800

    Remove packaging script.

--- package.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rd /s /q build dist melobot.egg-info
-python setup.py sdist bdist_wheel
--- package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf build dist melobot.egg-info
-python3 setup.py sdist bdist_wheel
