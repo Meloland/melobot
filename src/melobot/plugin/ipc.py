d42ce87180acff037ab52d5cfdf5e253b75690a5
aicorein <aicorein@meloland.org>
2024-07-30 19:34:59 +0800

    [melobot-dev] Add sync share object

--- src/melobot/plugin/ipc.py
+++ src/melobot/plugin/ipc.py
@@ -3,26 +3,28 @@ from dataclasses import dataclass
 
 from ..exceptions import BotIpcError
 from ..log import BotLogger
-from ..typing import TYPE_CHECKING, Any, AsyncCallable, Generic, T
+from ..typing import TYPE_CHECKING, Any, AsyncCallable, Callable, Generic, T
 from ..utils import RWContext
 
 if TYPE_CHECKING:
     from .base import Plugin
 
 
-class ShareObject(Generic[T]):
-    """共享对象"""
+class AsyncShare(Generic[T]):
+    """异步共享对象"""
 
     def __init__(
         self,
         name: str,
         reflector: AsyncCallable[[], T] | None = None,
         callabck: AsyncCallable[[T], None] | None = None,
+        static: bool = False,
     ) -> None:
         self.name = name
         self.__safe_ctx = RWContext()
         self.__reflect = reflector
         self.__callback = callabck
+        self._static = static
 
     def __call__(self, func: AsyncCallable[[], T]) -> AsyncCallable[[], T]:
         if self.__reflect is not None:
@@ -49,6 +51,44 @@ class ShareObject(Generic[T]):
             return await self.__callback(val)
 
 
+class SyncShare(Generic[T]):
+    """同步共享对象"""
+
+    def __init__(
+        self,
+        name: str,
+        reflector: Callable[[], T] | None = None,
+        callabck: Callable[[T], None] | None = None,
+        static: bool = False,
+    ) -> None:
+        self.name = name
+        self.__reflect = reflector
+        self.__callback = callabck
+        self._static = static
+
+    def __call__(self, func: Callable[[], T]) -> Callable[[], T]:
+        if self.__reflect is not None:
+            raise BotIpcError("共享对象已经有获取值的反射方法，不能再次绑定")
+        self.__reflect = func
+        return func
+
+    def setter(self, func: Callable[[T], None]) -> Callable[[T], None]:
+        if self.__callback is not None:
+            raise BotIpcError("共享对象已经有更新值的回调方法，不能再次绑定")
+        self.__callback = func
+        return func
+
+    def get(self) -> T:
+        if self.__reflect is None:
+            raise BotIpcError("共享对象获取值的反射方法未绑定")
+        return self.__reflect()
+
+    def set(self, val: T) -> None:
+        if self.__callback is None:
+            raise BotIpcError("共享对象更新值的回调方法未绑定")
+        self.__callback(val)
+
+
 @dataclass
 class SignalHandler:
     plugin: "Plugin"
@@ -58,17 +98,17 @@ class SignalHandler:
 
 class IPCManager:
     def __init__(self, logger: BotLogger) -> None:
-        self.shares: dict["Plugin", dict[str, ShareObject]] = {}
+        self.shares: dict["Plugin", dict[str, AsyncShare | SyncShare]] = {}
         self.handlers: dict["Plugin", dict[str, SignalHandler]] = {}
         self.logger = logger
 
-    def add_share(self, plugin: "Plugin", obj: ShareObject) -> None:
+    def add_share(self, plugin: "Plugin", obj: AsyncShare | SyncShare) -> None:
         objs = self.shares.setdefault(plugin, {})
         if objs.get(obj.name) is not None:
             raise BotIpcError(f"插件 {plugin.name} 中已存在名为 {obj.name} 的共享对象")
         objs[obj.name] = obj
 
-    def get_share(self, plugin: "Plugin", id: str) -> ShareObject:
+    def get_share(self, plugin: "Plugin", id: str) -> AsyncShare | SyncShare:
         if (objs := self.shares.get(plugin)) is None:
             raise BotIpcError(f"插件 {plugin.name} 中不存在名为 {id} 的共享对象")
         if (obj := objs.get(id)) is None:
