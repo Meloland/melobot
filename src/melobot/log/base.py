d4f2e7e7563db1f77722622f150732fc7049add7
aicorein <aicorein@meloland.org>
2024-10-12 21:02:23 +0800

    [melobot] Logger feature enhancements - Fix: Smart highlighting run with not needed condition. - Add: Support for log levels above ERROR, messages no longer smart highlighted, but all turn red. - Add: Support for v2-style log message colors, instead of smart highlighting, use five colors. - Add: The levels of console log and file log can be adjusted independently - Add: Two modes for file log: merge all outputs (default), stdout and stderr are seperated (similar to nginx)

--- src/melobot/log/base.py
+++ src/melobot/log/base.py
@@ -20,33 +20,46 @@ import colorlog
 import rich.console
 import rich.pretty
 from better_exceptions import ExceptionFormatter
-from rich.highlighter import ReprHighlighter
+from rich.highlighter import Highlighter, ReprHighlighter
+from rich.style import Style
 from rich.text import Text
 
-from ..typ import BetterABC, T, abstractmethod
+from ..typ import BetterABC, T, VoidType, abstractmethod
 from ..utils import singleton
 
 _CONSOLE_IO = io.StringIO()
 _CONSOLE = rich.console.Console(file=_CONSOLE_IO, record=True, color_system="256")
 _REPR_HIGHLIGHTER = ReprHighlighter()
-_HIGH_LIGHTWORDS = [
-    "GET",
-    "POST",
-    "HEAD",
-    "PUT",
-    "DELETE",
-    "OPTIONS",
-    "TRACE",
-    "PATCH",
-    "MELOBOT",
-    "melobot",
-]
+_HIGH_LIGHTWORDS = ["GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH"]
 
 
-def _get_rich_object(obj: object, max_len: Optional[int] = 2000) -> tuple[str, str]:
+class _StyleHighlighter(Highlighter):
+    def __init__(self, style: Style | None) -> None:
+        super().__init__()
+        self.style = style
+
+    def highlight(self, text: Text) -> None:
+        if self.style:
+            text.stylize(self.style)
+
+
+def _get_rich_object(
+    obj: object,
+    max_len: Optional[int] = 2000,
+    style: Style | None = None,
+    no_color: bool = False,
+) -> tuple[str, str]:
+    if no_color:
+        hl = _StyleHighlighter(style=None)
+    elif style:
+        hl = _StyleHighlighter(style)
+    else:
+        hl = None
+
     _CONSOLE.print(
         rich.pretty.Pretty(
             obj,
+            highlighter=hl,
             indent_guides=True,
             max_string=max_len,
             overflow="ignore",
@@ -60,9 +73,17 @@ def _get_rich_object(obj: object, max_len: Optional[int] = 2000) -> tuple[str, s
     return colored_str, _CONSOLE.export_text().rstrip("\n")
 
 
-def _get_rich_repr(s: str) -> tuple[str, str]:
-    msg = _REPR_HIGHLIGHTER(Text(s))
-    msg.highlight_words(_HIGH_LIGHTWORDS, "logging.keyword")
+def _get_rich_repr(
+    s: str, style: Style | None = None, no_color: bool = False
+) -> tuple[str, str]:
+    if no_color:
+        msg = Text(s)
+    elif style:
+        msg = Text(s, style=style)
+    else:
+        msg = _REPR_HIGHLIGHTER(Text(s))
+        msg.highlight_words(_HIGH_LIGHTWORDS, "logging.keyword")
+
     _CONSOLE.print(msg)
     colored_str = _CONSOLE_IO.getvalue()[:-1]
     _CONSOLE_IO.seek(0)
@@ -74,17 +95,26 @@ _EXC_FORMATTER = ExceptionFormatter(colored=True)
 _NO_COLOR_EXC_FORMATTER = ExceptionFormatter(colored=False)
 
 
-class MeloFilter(logging.Filter):
-    def __init__(self, name: str = "") -> None:
+class _NoErrFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return logging.DEBUG <= record.levelno < logging.ERROR
+
+
+class _MeloLogFilter(logging.Filter):
+    def __init__(
+        self, name: str = "", red_error: bool = True, legacy: bool = False
+    ) -> None:
         super().__init__(name)
-        self.obj = ""
-        self.colored_obj = ""
+        self._obj: Any = VoidType.VOID
+        self._red_style = Style(color="red")
+        self._enable_red_error = red_error
+        self._legacy = legacy
 
     def set_obj(self, obj: Any) -> None:
-        self.colored_obj, self.obj = _get_rich_object(obj)
+        self._obj = obj
 
     def clear_obj(self) -> None:
-        self.colored_obj, self.obj = "", ""
+        self._obj = VoidType.VOID
 
     @contextmanager
     def on_obj(self, obj: Any) -> Generator[None, None, None]:
@@ -99,10 +129,42 @@ class MeloFilter(logging.Filter):
         if record.args:
             msg = msg % record.args
         record.msg_str = msg
-        record.colored_msg_str, record.msg_str = _get_rich_repr(msg)
+
         record.mod_name, record.func_name, record.func_lineno = _current_finfo()
-        record.colored_obj, record.obj = self.colored_obj, self.obj
+        self._fill_msg_and_obj(msg, record)
         return True
+
+    def _fill_msg_and_obj(self, msg: str, record: logging.LogRecord) -> None:
+        red_style = self._red_style
+        red_error = self._enable_red_error
+
+        if self._legacy:
+            record.legacy_msg_str, record.colored_msg_str, record.msg_str = msg, "", msg
+
+            if self._obj is VoidType.VOID:
+                record.legacy_obj, record.obj = "", ""
+            else:
+                record.legacy_obj = record.obj = _get_rich_object(
+                    self._obj, no_color=True
+                )[1]
+            record.colored_obj = ""
+            return
+
+        record.legacy_msg_str = ""
+        record.legacy_obj = ""
+
+        if red_error and record.levelno >= ERROR:
+            record.colored_msg_str, record.msg_str = _get_rich_repr(msg, red_style)
+        else:
+            record.colored_msg_str, record.msg_str = _get_rich_repr(msg)
+
+        if self._obj is VoidType.VOID:
+            record.colored_obj, record.obj = "", ""
+        elif red_error and record.levelno >= ERROR:
+            record.legacy_obj = record.obj = _get_rich_object(self._obj, no_color=True)[1]
+            record.colored_obj = ""
+        else:
+            record.colored_obj, record.obj = _get_rich_object(self._obj)
 
 
 class LogLevel(int, Enum):
@@ -258,17 +320,28 @@ class Logger(_Logger, GenericLogger):
         name: str = "melobot",
         /,
         level: LogLevel = LogLevel.INFO,
+        file_level: LogLevel = LogLevel.DEBUG,
         to_console: bool = True,
         to_dir: Optional[str] = None,
-        no_tag: bool = True,
+        add_tag: bool = True,
+        legacy: bool = False,
+        red_error: bool = True,
+        two_stream: bool = False,
     ) -> None:
         """初始化日志器
 
         :param name: 日志器的名称（唯一）
         :param level: 日志等级
-        :param to_console: 是否输出到 console
+        :param file_level: 日志文件的日志等级
+        :param to_console: 是否输出到控制台
         :param to_dir: 保存日志文件的目录，为空则不保存文件
-        :param no_tag: 记录日志时是否不标识日志器名称
+        :param add_tag: 记录日志时是否标识日志器名称
+        :param legacy: 记录日志时是否使用传统样式（不对日志内容进行自动高亮，而是使用日志等级的五色）
+        :param red_error:
+            记录 `LogLevel.ERROR` 及以上级别时，是否将日志内容着色为红色。
+            `legacy` 选项为 `True` 时此参数无效
+
+        :param two_stream: 当使用记录到文件功能时，是否分离“常规日志”和“错误日志”到不同的文件
         """
         if (
             hasattr(self, "_built")
@@ -276,35 +349,45 @@ class Logger(_Logger, GenericLogger):
         ):
             return
 
-        super().__init__(name, level)
-        self._con_handler: Optional[logging.Handler] = None
+        super().__init__(name, LogLevel.DEBUG)
         self._handler_arr: list[logging.Handler] = []
-        self._no_tag = no_tag
-        self._filter = MeloFilter(name)
+        self._no_tag = not add_tag
+        self._filter = _MeloLogFilter(name, red_error, legacy)
 
         if to_console:
-            self._add_console_handler()
-        if to_dir is not None:
-            self._add_file_handler(to_dir)
+            con_handler = self._add_console_handler()
+            con_handler.setLevel(level)
+
+        if to_dir is None:
+            pass
+        elif not two_stream:
+            self._add_file_handler(to_dir, name, file_level)
+        else:
+            normal_handler = self._add_file_handler(to_dir, f"{name}.out", file_level)
+            normal_handler.addFilter(_NoErrFilter(name))
+            self._add_file_handler(to_dir, f"{name}.err", max(file_level, LogLevel.ERROR))
 
         self._built: bool = True
 
-    def _add_console_handler(self) -> None:
-        if self._con_handler is None:
-            fmt = self._console_fmt(self.name, self._no_tag)
-            self._con_handler = self._console_handler(fmt)
-            self._con_handler.addFilter(self._filter)
-
-            self.addHandler(self._con_handler)
-            self._handler_arr.append(self._con_handler)
-
-    def _add_file_handler(self, log_dir: str) -> None:
-        fmt = self._file_fmt(self.name, self._no_tag)
-        handler = self._file_handler(fmt, log_dir, self.name)
+    def _add_console_handler(self) -> logging.Handler:
+        fmt = self._console_fmt(self.name, self._no_tag)
+        handler = self._console_handler(fmt)
         handler.addFilter(self._filter)
 
         self.addHandler(handler)
         self._handler_arr.append(handler)
+        return handler
+
+    def _add_file_handler(
+        self, log_dir: str, name: str, level: LogLevel = LogLevel.DEBUG
+    ) -> logging.Handler:
+        fmt = self._file_fmt(self.name, self._no_tag)
+        handler = self._file_handler(fmt, log_dir, name, level)
+        handler.addFilter(self._filter)
+
+        self.addHandler(handler)
+        self._handler_arr.append(handler)
+        return handler
 
     @staticmethod
     def _make_fmt_nocache(fmt: logging.Formatter) -> None:
@@ -319,7 +402,7 @@ class Logger(_Logger, GenericLogger):
     @staticmethod
     def _console_fmt(name: str, no_tag: bool = False) -> logging.Formatter:
         fmt_arr = [
-            "%(green)s%(asctime)s.%(msecs)03d%(reset)s",
+            "%(cyan)s%(asctime)s.%(msecs)03d%(reset)s",
             "%(log_color)s%(levelname)-7s%(reset)s",
             "%(blue)s%(mod_name)s%(reset)s:%(blue)s%(func_name)s%(reset)s"
             + ":%(cyan)s%(func_lineno)d%(reset)s",
@@ -327,7 +410,9 @@ class Logger(_Logger, GenericLogger):
         if not no_tag:
             fmt_arr.insert(0, f"%(purple)s{name}%(reset)s")
         fmt_s = " | ".join(fmt_arr)
-        fmt_s += " - %(colored_msg_str)s%(colored_obj)s"
+        msg_str_f = "%(log_color)s%(legacy_msg_str)s%(reset)s%(colored_msg_str)s"
+        obj_str_f = "%(log_color)s%(legacy_obj)s%(reset)s%(colored_obj)s"
+        fmt_s += f" - {msg_str_f}{obj_str_f}"
 
         fmt = colorlog.ColoredFormatter(fmt_s, datefmt="%Y-%m-%d %H:%M:%S", reset=True)
         fmt.default_msec_format = "%s.%03d"
@@ -370,7 +455,9 @@ class Logger(_Logger, GenericLogger):
         return fmt
 
     @staticmethod
-    def _file_handler(fmt: logging.Formatter, log_dir: str, name: str) -> logging.Handler:
+    def _file_handler(
+        fmt: logging.Formatter, log_dir: str, name: str, level: LogLevel
+    ) -> logging.Handler:
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
 
@@ -380,19 +467,21 @@ class Logger(_Logger, GenericLogger):
             backupCount=10,
             encoding="UTF-8",
         )
+        handler.setLevel(level)
         handler.setFormatter(fmt)
         return handler
 
     def set_level(self, level: LogLevel) -> None:  # type: ignore[override]
         """设置日志等级
 
-        日志等级自动应用于包含的所有 handler
+        日志等级自动应用于包含的所有 handler（但输出日志到文件的 handler 除外）
 
         :param level: 日志等级
         """
         super().setLevel(level)
         for handler in self._handler_arr:
-            handler.setLevel(level)
+            if not isinstance(handler, logging.handlers.RotatingFileHandler):
+                handler.setLevel(level)
 
     def findCaller(
         self, stack_info: bool = False, stacklevel: int = 1
