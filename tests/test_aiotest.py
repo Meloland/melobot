719e1cf742c80718912684a2c75a1d67be661386
Snowykami <snowykami@outlook.com>
2024-12-24 19:32:12 +0800

    :art: apple pre-commit check

--- tests/test_aiotest.py
+++ tests/test_aiotest.py
@@ -11,16 +11,12 @@ async def test_v() -> int:
     return 5
 
 
-async def test_loop(
-    current_loop: aio.AbstractEventLoop, int_val: int
-) -> None:
+async def test_loop(current_loop: aio.AbstractEventLoop, int_val: int) -> None:
     assert int_val == 1
     assert current_loop is aio.get_running_loop()
 
 
-async def test_loop2(
-    current_loop: aio.AbstractEventLoop, test_v: int
-) -> None:
+async def test_loop2(current_loop: aio.AbstractEventLoop, test_v: int) -> None:
     assert test_v == 5
     assert current_loop is aio.get_running_loop()
 
