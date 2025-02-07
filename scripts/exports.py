e7ddf804b9c453c4f7c1972aa22596c955511362
aicorein <aicorein@meloland.org>
2025-02-07 12:08:41 +0800

    [melobot] Use new standards in pyprojects & updated dependencies

--- scripts/exports.py
+++ scripts/exports.py
@@ -8,12 +8,11 @@ with open(r"pyproject.toml", "r", encoding="utf-8") as fp:
     pyproj_toml = toml.load(fp)
 
 except_groups = (
-    pyproj_toml["tool"]["pdm"]["dev-dependencies"]
-    | pyproj_toml["project"]["optional-dependencies"]
+    pyproj_toml["dependency-groups"] | pyproj_toml["project"]["optional-dependencies"]
 )
 
 
-def get_nega_groups(*groups: str | None) -> str:
+def get_complement_groups(*groups: str | None) -> str:
     gs = except_groups.copy()
     for g in groups:
         if g is not None:
@@ -23,7 +22,7 @@ def get_nega_groups(*groups: str | None) -> str:
 
 def main() -> None:
     ret = os.system(
-        f"pdm export -o tests/requirements.txt --without {get_nega_groups('test', 'onebot')}"
+        f"pdm export -o tests/requirements.txt --without {get_complement_groups('test', 'onebot')}"
     )
     if ret == 0:
         print("已刷新项目测试的 requirements.txt")
@@ -31,7 +30,7 @@ def main() -> None:
         print("未能完成刷新项目测试的 requirements.txt 的任务")
 
     ret = os.system(
-        f"pdm export -o docs/requirements.txt --without {get_nega_groups('docs', 'onebot')}"
+        f"pdm export -o docs/requirements.txt --without {get_complement_groups('docs', 'onebot')}"
     )
     if ret == 0:
         print("已刷新项目文档构建的 requirements.txt")
