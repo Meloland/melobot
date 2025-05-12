8c63d993ec8163fbfe6d4db139b23b294a9133e6
aicorein <aicorein@meloland.org>
2025-05-12 15:09:04 +0800

    [melobot] Updated version flag

--- src/melobot/_meta.py
+++ src/melobot/_meta.py
@@ -6,7 +6,7 @@ from typing_extensions import Any, ClassVar, Generic, Literal, NamedTuple, NoRet
 
 from .typ.base import T
 
-__version__ = "3.2.1"
+__version__ = "3.2.2"
 
 
 def _version_str_to_info(s: str) -> VersionInfo:
